66a0680059e64c262553d5f81083df9bd5c51c2d
hin101 <hineshpatel0@googlemail.com>
2017-11-04 20:39:54 +1100

    Refactor grade school

--- grade-school/grade_school.rb
+++ grade-school/grade_school.rb
@@ -1,33 +1,19 @@
 class School
   def initialize
-    @students = []
+    @students = Hash.new{ |h,k| h[k] = [] }
   end
 
   def add(student, grade)
-    grade_exists = false
-    @students.each do |c|
-      if c.value?(grade)
-        x = c[:students]
-        x << student
-        c[:students] = x.sort
-        return
-      end
-    end
-    @students << { grade: grade, students: [student].sort }
+    @students[grade] << student
   end
 
   def students(grade)
-    students = []
-    @students.each do |c|
-      if c.value?(grade)
-        students = c[:students]
-      end
-    end
-    students.sort
+    @students[grade].sort
   end
 
   def students_by_grade
-    @students.sort_by { |hsh| hsh[:grade] }
+    return [] if @students.empty?
+    @students.sort.map { |grade, names| { grade: grade, students: names.sort } }
   end
 end
 
