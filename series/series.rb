15c023c5cdf326df266f247da5aae5df89393da7
hin101 <hineshpatel0@googlemail.com>
2017-11-04 21:05:09 +1100

    Solution for series exercise in exercism.io

---
+++ series/series.rb
@@ -0,0 +1,10 @@
+class Series
+  def initialize(digits)
+    @digits = digits.chars.map(&:to_i)
+  end
+
+  def slices(count)
+    raise ArgumentError if count > @digits.count
+    @digits.each_cons(count).to_a.map { |d| d * "" }
+  end
+end
