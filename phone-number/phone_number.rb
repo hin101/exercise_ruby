24c8f7c59a8042d708204308be30ef16b5d5b03c
hin101 <hineshpatel0@googlemail.com>
2017-11-05 19:49:42 +1100

    solution for phone number exercise

---
+++ phone-number/phone_number.rb
@@ -0,0 +1,11 @@
+# phone number
+class PhoneNumber
+  def self.clean(phone_number)
+    clean_number = phone_number.gsub(/\D/, "").gsub(/^1/,"")
+    return clean_number if(("2".."9") === clean_number[3] && clean_number.length == 10 )
+  end
+end
+
+module BookKeeping
+  VERSION = 2
+end
